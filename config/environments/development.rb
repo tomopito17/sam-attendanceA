51956767f5173ea79c75a7a2056b5c4f8510cebd
Happy Yuriko <yuriko-ito@hotmail.co.jp>
2018-12-30 16:08:57 +0000

    update account activate for kinati-a-cloned

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,6 +54,6 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'kintai-a-happyyuriko.c9users.io'     # クラウドIDE
+  host = 'kintai-a-cloned-happyyuriko.c9users.io'     # クラウドIDE
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 end
