c7aef1df874c72a3c760829b3623d6b116a4bbbd
Happy Yuriko <yuriko-ito@hotmail.co.jp>
2019-01-13 04:01:38 +0000

    Finish attendance_users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
+      flash[:info] = "Emailを確認してアカウントの有効化をクリックしてください。"
       redirect_to root_url
     else
       render 'new'
